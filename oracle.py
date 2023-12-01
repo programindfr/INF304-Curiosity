9fb49b5d568899879627b0892c24ecb3d1239a28
ArtichautCosmique <lelolartichaut@laposte.net>
2023-12-01 18:40:32 +0100

    meilleur oracle et bons dossiers

--- oracle.py
+++ oracle.py
@@ -3,7 +3,19 @@
 from os import listdir
 from subprocess import getoutput
 
-for test in listdir("Terrains"):
-	print(f"Test {test}")
-	out = getoutput(f"./test_terrain Terrains/{test}").split("\n")[1][0]
-	print("[PASS]" if test[-1] == out else "[FAIL]")
+RESET = "\033[0m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+
+def test_terrain(folder, filename):
+	out = getoutput(f"./test_terrain {folder}/{filename}").split("\n")[1][0]
+	print(f"Test {test}", GREEN+"[PASS]"+RESET if test[-1] == out else RED+"[FAIL]"+RESET)
+
+print(YELLOW+"##### Terrains corrects #####"+RESET)
+for test in listdir("Terrains_Corrects"):
+	test_terrain("Terrains_Corrects", test)
+
+print(YELLOW+"#### Terrains incorrects ####"+RESET)
+for test in listdir("Terrains_Incorrects"):
+	test_terrain("Terrains_Incorrects", test)
